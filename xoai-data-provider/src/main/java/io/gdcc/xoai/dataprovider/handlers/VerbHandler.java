6c14ad0784de62d60c2c9feec718f559f3e1b8bb
Oliver Bertuch <o.bertuch@fz-juelich.de>
2022-06-07 23:53:58 +0200

    refactor(data): make request analysis for sets, format and filter reusable
    - The VerbHandler now offers verification of a set parameter, extracting and
      verifying the metadata format against the context and create the list of filters
      from context, format and sets.
    - This makes handlers methods much more dense, thus easier to read and comprehend.

--- xoai-data-provider/src/main/java/io/gdcc/xoai/dataprovider/handlers/VerbHandler.java
+++ xoai-data-provider/src/main/java/io/gdcc/xoai/dataprovider/handlers/VerbHandler.java
@@ -8,13 +8,23 @@
 
 package io.gdcc.xoai.dataprovider.handlers;
 
+import io.gdcc.xoai.dataprovider.exceptions.handler.CannotDisseminateFormatException;
+import io.gdcc.xoai.dataprovider.exceptions.handler.DoesNotSupportSetsException;
 import io.gdcc.xoai.dataprovider.exceptions.handler.HandlerException;
+import io.gdcc.xoai.dataprovider.exceptions.handler.NoMatchesException;
+import io.gdcc.xoai.dataprovider.filter.ScopedFilter;
 import io.gdcc.xoai.dataprovider.model.Context;
+import io.gdcc.xoai.dataprovider.model.MetadataFormat;
+import io.gdcc.xoai.dataprovider.model.Set;
 import io.gdcc.xoai.dataprovider.repository.Repository;
 import io.gdcc.xoai.dataprovider.repository.RepositoryConfiguration;
 import io.gdcc.xoai.model.oaipmh.Request;
 import io.gdcc.xoai.model.oaipmh.ResumptionToken;
 import io.gdcc.xoai.model.oaipmh.verbs.Verb;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public abstract class VerbHandler<T extends Verb> {
     private final Context context;
@@ -33,6 +43,50 @@ public abstract class VerbHandler<T extends Verb> {
     }
     protected RepositoryConfiguration getConfiguration() {
         return repository.getConfiguration();
+    }
+    
+    /**
+     * Check for set support if set argument is present and check set existence (in both set repository and
+     * context's static defined sets!).
+     *
+     * @param request The request from the client
+     * @throws HandlerException When sets aren't supported or the set does not exist.
+     */
+    protected void verifySet(Request request) throws HandlerException {
+        Optional<String> requestedSet = request.getSet();
+        if (requestedSet.isPresent()) {
+            if (!getRepository().getSetRepository().supportSets())
+                throw new DoesNotSupportSetsException();
+            else if (!getRepository().getSetRepository().exists(requestedSet.get()) && !getContext().hasSet(requestedSet.get()))
+                throw new NoMatchesException("Requested set '" + requestedSet.get() + "' does not exist");
+        }
+    }
+    
+    protected MetadataFormat verifyFormat(Request request) throws HandlerException {
+        // Get the metadata format or throw errors
+        final String requestedFormat = request.getMetadataPrefix()
+            .orElseThrow(() -> new CannotDisseminateFormatException("Missing required argument 'metadataPrefix'"));
+        return Optional.ofNullable(getContext().formatForPrefix(requestedFormat))
+            .orElseThrow(() -> new CannotDisseminateFormatException("Format '" + requestedFormat + "' not applicable in this context"));
+    }
+    
+    protected List<ScopedFilter> createFilters(Request request, MetadataFormat format) throws HandlerException {
+        // Create empty filter list
+        final List<ScopedFilter> filters = new ArrayList<>();
+        
+        // Create the filter from the context and add to list (will default to being transparent)
+        filters.add(getContext().getScopedFilter());
+        
+        // Add the metadata formats condition to the list of filters (will default to being transparent)
+        filters.add(format.getScopedFilter());
+        
+        // Add the sets condition if the requested set is contained within the context
+        request.getSet()
+            .flatMap(setSpec -> getContext().getSet(setSpec))
+            .map(Set::getScopedFilter)
+            .map(filters::add);
+        
+        return filters;
     }
     
     /**
