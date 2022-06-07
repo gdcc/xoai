80ad626ad5b71c353be6afb68c192d9eef10b1d4
Oliver Bertuch <o.bertuch@fz-juelich.de>
2022-06-07 23:57:30 +0200

    refactor(data): migrate ListIdentifiersHandler
    With all things in place, this handler now
    - uses the VerbHandler methods to do verification and filter creation
    - benefits from the much easier to use pages results
    - handles the resumption token via the result

--- xoai-data-provider/src/test/java/io/gdcc/xoai/dataprovider/handlers/ListIdentifiersHandlerTest.java
+++ xoai-data-provider/src/test/java/io/gdcc/xoai/dataprovider/handlers/ListIdentifiersHandlerTest.java
@@ -8,15 +8,15 @@
 
 package io.gdcc.xoai.dataprovider.handlers;
 
-import io.gdcc.xoai.dataprovider.exceptions.BadArgumentException;
-import io.gdcc.xoai.dataprovider.exceptions.CannotDisseminateFormatException;
-import io.gdcc.xoai.dataprovider.exceptions.DoesNotSupportSetsException;
-import io.gdcc.xoai.dataprovider.exceptions.NoMatchesException;
+import io.gdcc.xoai.dataprovider.exceptions.handler.CannotDisseminateFormatException;
+import io.gdcc.xoai.dataprovider.exceptions.handler.DoesNotSupportSetsException;
+import io.gdcc.xoai.dataprovider.exceptions.handler.NoMatchesException;
 import io.gdcc.xoai.dataprovider.model.MetadataFormat;
+import io.gdcc.xoai.exceptions.BadArgumentException;
 import org.junit.jupiter.api.Test;
 
 import static io.gdcc.xoai.dataprovider.model.InMemoryItem.randomItem;
-import static io.gdcc.xoai.model.oaipmh.Verb.Type.ListIdentifiers;
+import static io.gdcc.xoai.model.oaipmh.verbs.Verb.Type.ListIdentifiers;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -26,7 +26,7 @@ public class ListIdentifiersHandlerTest extends AbstractHandlerTest {
 
     @Test
     public void metadataPrefixIsMandatory () {
-        assertThrows(BadArgumentException.class, () -> underTest.handle(a(request().withVerb(ListIdentifiers))));
+        assertThrows(BadArgumentException.class, () -> underTest.handle(request().withVerb(ListIdentifiers)));
     }
 
     @Test
@@ -34,31 +34,31 @@ public class ListIdentifiersHandlerTest extends AbstractHandlerTest {
         theItemRepository().withItem(randomItem().withIdentifier("1"));
         aContext().withMetadataFormat(EXISTING_METADATA_FORMAT, MetadataFormat.identity());
         assertThrows(CannotDisseminateFormatException.class,
-            () -> underTest.handle(a(request().withVerb(ListIdentifiers).withMetadataPrefix("abcd"))));
+            () -> underTest.handle(request().withVerb(ListIdentifiers).withMetadataPrefix("abcd")));
     }
 
     @Test
     public void doesNotSupportSets() {
         theSetRepository().doesNotSupportSets();
         assertThrows(DoesNotSupportSetsException.class,
-            () -> underTest.handle(a(request()
+            () -> underTest.handle(request()
                     .withVerb(ListIdentifiers)
                     .withMetadataPrefix(EXISTING_METADATA_FORMAT)
-                    .withSet("hello"))));
+                    .withSet("hello")));
     }
 
     @Test
     public void responseWithoutItems()  {
         assertThrows(NoMatchesException.class,
-            () -> underTest.handle(a(request()
+            () -> underTest.handle(request()
                     .withVerb(ListIdentifiers)
-                    .withMetadataPrefix(EXISTING_METADATA_FORMAT))));
+                    .withMetadataPrefix(EXISTING_METADATA_FORMAT)));
     }
 
     @Test
     public void responseWithItems () throws Exception {
         theItemRepository().withRandomItems(10);
-        String result = write(underTest.handle(a(request().withVerb(ListIdentifiers).withMetadataPrefix(EXISTING_METADATA_FORMAT))));
+        String result = write(underTest.handle(request().withVerb(ListIdentifiers).withMetadataPrefix(EXISTING_METADATA_FORMAT)));
 
         assertThat(result, xPath("count(//header)", asInteger(equalTo(10))));
     }
