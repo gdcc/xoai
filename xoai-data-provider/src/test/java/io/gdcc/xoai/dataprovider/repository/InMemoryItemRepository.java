94a9b027b95d8f5ae3d6187b0a0ccd0fedabcdcd
Oliver Bertuch <o.bertuch@fz-juelich.de>
2022-06-08 11:42:12 +0200

    refactor(data): adapt testing repository implementations
    The InMemory style item and set repositories need to be aligned with
    the changed interface definitions. Using Stream API on the lists to
    create the paged results.

--- xoai-data-provider/src/test/java/io/gdcc/xoai/dataprovider/repository/InMemoryItemRepository.java
+++ xoai-data-provider/src/test/java/io/gdcc/xoai/dataprovider/repository/InMemoryItemRepository.java
@@ -8,21 +8,20 @@
 
 package io.gdcc.xoai.dataprovider.repository;
 
+import io.gdcc.xoai.dataprovider.exceptions.handler.HandlerException;
+import io.gdcc.xoai.dataprovider.exceptions.handler.IdDoesNotExistException;
+import io.gdcc.xoai.dataprovider.filter.ScopedFilter;
 import io.gdcc.xoai.dataprovider.model.InMemoryItem;
 import io.gdcc.xoai.dataprovider.model.Item;
-import io.gdcc.xoai.dataprovider.exceptions.handler.IdDoesNotExistException;
-import io.gdcc.xoai.exceptions.OAIException;
-import io.gdcc.xoai.dataprovider.filter.ScopedFilter;
-import io.gdcc.xoai.dataprovider.handlers.results.ListItemIdentifiersResult;
-import io.gdcc.xoai.dataprovider.handlers.results.ListItemsResults;
+import io.gdcc.xoai.dataprovider.model.ItemIdentifier;
 import io.gdcc.xoai.dataprovider.model.MetadataFormat;
+import io.gdcc.xoai.model.oaipmh.ResumptionToken;
 
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static io.gdcc.xoai.dataprovider.model.InMemoryItem.randomItem;
-import static java.lang.Math.min;
 import static java.util.Arrays.asList;
 
 public class InMemoryItemRepository implements ItemRepository {
@@ -49,7 +48,7 @@ public class InMemoryItemRepository implements ItemRepository {
     }
 
     @Override
-    public Item getItem(String identifier) throws IdDoesNotExistException, OAIException {
+    public Item getItem(String identifier) throws IdDoesNotExistException {
         for (InMemoryItem item : this.list) {
             if (item.getIdentifier().equals(identifier))
                 return item;
@@ -58,87 +57,39 @@ public class InMemoryItemRepository implements ItemRepository {
     }
     
     @Override
-    public Item getItem(String identifier, MetadataFormat format) throws IdDoesNotExistException, OAIException {
+    public Item getItem(String identifier, MetadataFormat format) throws HandlerException {
         return getItem(identifier);
     }
     
     @Override
-    public ListItemIdentifiersResult getItemIdentifiers(List<ScopedFilter> filters, int offset, int length) throws OAIException {
-        return new ListItemIdentifiersResult(offset + length < list.size(), new ArrayList<>(list.subList(offset, min(offset + length, list.size()))));
+    public ResultsPage<ItemIdentifier> getItemIdentifiers(List<ScopedFilter> filters, MetadataFormat metadataFormat, int maxResponseLength, ResumptionToken.Value resumptionToken) throws HandlerException {
+        List<ItemIdentifier> pagedResults = this.list.stream()
+            .skip(resumptionToken.getOffset())
+            .limit(maxResponseLength)
+            .collect(Collectors.toUnmodifiableList());
+    
+        return new ResultsPage<>(
+            resumptionToken,
+            // more only when page size = maxlength - but only when this is not also the end of the list (edge case where maxlength is a multiple of total size)
+            pagedResults.size() == maxResponseLength && this.list.size() != resumptionToken.getOffset() + maxResponseLength,
+            pagedResults,
+            list.size()
+        );
     }
-
+    
     @Override
-    public ListItemIdentifiersResult getItemIdentifiers(List<ScopedFilter> filters, int offset, int length, Instant from) throws OAIException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public ListItemIdentifiersResult getItemIdentifiersUntil(List<ScopedFilter> filters, int offset, int length, Instant until) throws OAIException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public ListItemIdentifiersResult getItemIdentifiers(List<ScopedFilter> filters, int offset, int length, Instant from, Instant until) throws OAIException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public ListItemIdentifiersResult getItemIdentifiers(List<ScopedFilter> filters, int offset, int length, String setSpec) throws OAIException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public ListItemIdentifiersResult getItemIdentifiers(List<ScopedFilter> filters, int offset, int length, String setSpec, Instant from) throws OAIException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public ListItemIdentifiersResult getItemIdentifiersUntil(List<ScopedFilter> filters, int offset, int length, String setSpec, Instant until) throws OAIException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public ListItemIdentifiersResult getItemIdentifiers(List<ScopedFilter> filters, int offset, int length, String setSpec, Instant from, Instant until) throws OAIException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public ListItemsResults getItems(List<ScopedFilter> filters, int offset, int length) throws OAIException {
-        return new ListItemsResults(offset + length < list.size(), new ArrayList<>(list.subList(offset, min(offset + length, list.size()))));
-    }
-
-    @Override
-    public ListItemsResults getItems(List<ScopedFilter> filters, int offset, int length, Instant from) throws OAIException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public ListItemsResults getItemsUntil(List<ScopedFilter> filters, int offset, int length, Instant until) throws OAIException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public ListItemsResults getItems(List<ScopedFilter> filters, int offset, int length, Instant from, Instant until) throws OAIException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public ListItemsResults getItems(List<ScopedFilter> filters, int offset, int length, String setSpec) throws OAIException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public ListItemsResults getItems(List<ScopedFilter> filters, int offset, int length, String setSpec, Instant from) throws OAIException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public ListItemsResults getItemsUntil(List<ScopedFilter> filters, int offset, int length, String setSpec, Instant until) throws OAIException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public ListItemsResults getItems(List<ScopedFilter> filters, int offset, int length, String setSpec, Instant from, Instant until) throws OAIException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    public ResultsPage<Item> getItems(List<ScopedFilter> filters, MetadataFormat metadataFormat, int maxResponseLength, ResumptionToken.Value resumptionToken) throws HandlerException {
+        List<Item> pagedResults = this.list.stream()
+            .skip(resumptionToken.getOffset())
+            .limit(maxResponseLength)
+            .collect(Collectors.toUnmodifiableList());
+    
+        return new ResultsPage<>(
+            resumptionToken,
+            // more only when page size = maxlength - but only when this is not also the end of the list (edge case where maxlength is a multiple of total size)
+            pagedResults.size() == maxResponseLength && this.list.size() != resumptionToken.getOffset() + maxResponseLength,
+            pagedResults,
+            list.size()
+        );
     }
 }
