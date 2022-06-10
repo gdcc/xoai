d07aa7feaa6e60cea6a7ca4ff8efa878df452c36
Oliver Bertuch <o.bertuch@fz-juelich.de>
2022-06-10 12:27:30 +0200

    test(data): remove orphaned test

--- xoai-data-provider/src/test/java/io/gdcc/xoai/dataprovider/request/OAIRequestTest.java
+++
@@ -1,9 +0,0 @@
-package io.gdcc.xoai.dataprovider.request;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class OAIRequestTest {
-    
-    // TODO: add tests for loading the resumption token here
-    
-}
